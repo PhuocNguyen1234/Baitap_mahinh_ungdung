871e5e23c2f91ffb2ffe56abfff322d811a21761
Nguyen Phuoc <nguyenphuoc0768@gmail.com>
2025-09-26 21:51:18 +0700

    change tab layout

--- app/src/main/java/com/example/manhinh_ungdung/SignupTabActivity.java
+++ app/src/main/java/com/example/manhinh_ungdung/SignupTabActivity.java
@@ -1,16 +1,30 @@
 package com.example.manhinh_ungdung;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class SignupTabFragment extends Fragment {
+public class SignupTabActivity extends AppCompatActivity {
+    Button btnback;
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.signup_tab, container, false);
-        return root;
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_tab);
+        btnback = (Button) findViewById(R.id.backfrsignup);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupTabActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
